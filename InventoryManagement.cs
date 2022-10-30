0c3f3e44151d9931af9aec258dfddeea320e121f
Shreema KN <shreemakn1013@gmail.com>
2022-10-31 04:03:51 +0530

    added items

---
+++ InventoryManagement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgram
+{
+    public class Inventory//model class
+    {
+        public TypeofRice typeofRice;
+
+        public TypeofWheat typeofWheat;
+
+        public TypeofPulse typeofPulse;
+
+
+
+    }
+    public class TypeofRice
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+    public class TypeofWheat
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+    public class TypeofPulse
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+
+}
+Footer
+}
